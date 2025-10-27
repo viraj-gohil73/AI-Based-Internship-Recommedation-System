6840fd64487f073b90152993da8c9a3764f4c7ab
code-with-vg <virajgohil245@gmail.com>
2025-10-27 17:05:59 +0530

    Creating and Setup the Basic File Structure For System

---
+++ README.md
@@ -0,0 +1,8 @@
+# 27-10-25
+
+## Ready Structure
+
+Creating Basic Structure of Frontend and Backend.
+
+## Install Package
+TailwindCSS, ExpressJs
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <h1 className='text-3xl font-bold underline'>InternVision</h1>
+  )
+}
+
+export default App
